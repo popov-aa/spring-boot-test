f51d05bfa0d1c4da8753e2edb07c9e624805b74a
Alexey Popov <popov.spb@gmail.com>
2020-09-23 17:17:29 +0300

    beans listing has added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.service.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var applicationContext = SpringApplication.run(DemoApplication.class, args);
+		for (var name: applicationContext.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
+		applicationContext.getBean(TestService.class);
 	}
 
 }
--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -7,12 +7,15 @@ import com.example.demo.entities.EntityA;
 import com.example.demo.repository.EntityARepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TestService {
 
     @PersistenceContext
